feeb189a928ddfe6c0d35f5f69826cdc36e84ee6
Dhananajayaweerakoon <128120353+Dhananajayaweerakoon@users.noreply.github.com>
2024-05-12 22:48:29 +0530

    NoteApp

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -1,36 +1,42 @@
 package com.example.notesapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.notesapp.databinding.ActivityMainBinding
+import android.view.LayoutInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-
+import com.example.notesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var db: Notesdb
-    private lateinit var noteAdapter: NoteAdapter
+    private lateinit var notesAdapter: NoteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-       /* db = Notesdb(this)*/
+
+        db = Notesdb(this)
+        notesAdapter = NoteAdapter(db.getAllNotes(), this)
 
         binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
-       /* binding.notesRecyclerView.adapter = noteAdapter*/
+        binding.notesRecyclerView.adapter = notesAdapter
 
-       /* noteAdapter = NoteAdapter(db.getAllNotes(),this)*/
         binding.addButton.setOnClickListener {
             val intent = Intent(this, Addnote::class.java)
             startActivity(intent)
         }
-    }
-    /*override fun onResume() {
-        super.onResume()
-        noteAdapter.refreshData(db.getAllNotes())
-    }*/
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
+    }
 
 }
